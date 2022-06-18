56bcf63a600ed4d038b3f83cf3a6230d330b5f36
kritika-kishore <71135912+kritika-kishore@users.noreply.github.com>
2022-06-18 18:06:37 +0530

    Initialized backend and frontend

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running..");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((noteWithId) => noteWithId._id === req.params.id);
+  res.send(note);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`server started on port ${PORT}`));
